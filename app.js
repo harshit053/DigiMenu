d350c3989b6ceaab983d9cebe8155f598f136c14
harshit053 <harshitgupta.gh53@gmail.com>
2021-07-26 21:36:33 +0530

    new route done

--- app.js
+++ app.js
@@ -16,17 +16,30 @@ app.use(express.static("public"));
 
 seedDB();
 
-
+// Landing Page
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+
+//Index Page
 app.get("/menu", function(req, res) {
     FoodType.find({}).populate("foods").exec(function(err, foodTypes) {
         if(err) {
             console.log(err);
         } else {
             res.render("index", {foodTypes: foodTypes});
+        }
+    });
+});
+
+//New Page
+app.get("/menu/new", function(req, res) {
+    FoodType.find({}, function(err, foodTypes) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("new", {foodTypes: foodTypes});
         }
     });
 });
